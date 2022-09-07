97b50ac8a7e4389507dec40dda0aea1c579f44cb
ttrss <43591752+ttrssreal@users.noreply.github.com>
2022-09-08 01:47:01 +1200

    ordr progress w/ render items. search

--- src/user/views.py
+++ src/user/views.py
@@ -3,6 +3,6 @@ from user.models import User
 
 def profile():
     if not session.get("authed"):
-        return redirect("/")
+        return redirect("/login?next=profile")
     user = User.query.filter_by(student_id=int(session["studentid"])).first()
     return render_template("user/profile.html", session=session, studentid=user.student_id , first_name=user.first_name, last_name=user.last_name)
---
+++ static/js/order/order.js
@@ -0,0 +1,50 @@
+window.onload = () => {
+    var items = null;
+    var filtered_items = null;
+    var search_element = document.getElementById("seach-box");
+    var form = document.querySelector(".form-elements form");
+    var item_list_element = document.querySelector(".item-list");
+
+    search_element.onkeyup = ev => {
+        var searched = search_element.value;
+        filtered_items = items.filter(item => { return item.name.includes(searched) });
+        refresh_list();
+    }
+
+    function refresh_list() {
+        item_list_element.innerHTML = "";
+        filtered_items.forEach(item => {
+            var div = document.createElement("div");
+            div.className = "searched-item";
+            var name = document.createElement("p");
+            name.className = "name";
+            var price = document.createElement("p");
+            price.className = "price";
+            name.innerHTML = item.name;
+            price.innerHTML = "$" + item.price;
+            div.append(name);
+            div.append(price);
+            item_list_element.append(div);
+        });
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    xhr.open("POST", "/order");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify({"action": "get_items"}));
+    xhr.onreadystatechange = () => {
+        if(xhr.readyState == 4 && xhr.status == 200) {
+            var body = JSON.parse(xhr.responseText);
+            switch (body["status"]) {
+                case "error":
+                    alert("Error talking to the server. " + body["msg"])
+                    break;
+                case "success":
+                    items = body["items"];
+                    filtered_items = items
+                    refresh_list();
+            }
+        }
+    }
+}
